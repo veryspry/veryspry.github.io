a300eb1aca9744c5457f4410253a6f41273c02bd
veryspry <ehlinger.matt@gmail.com>
2017-11-22 11:50:45 -0600

    quiz reset update

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,13 +1,17 @@
+//Hide Reset buttonuntil we are ready for it later
+//It is before the document.ready statement so that user can't see it
+$('#reset').hide();
+
 $(document).ready( function() {
 
 //This function formats a given string to have a capital first character followed by all lowercase characters
 function capitolFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
-
+ 
 //when the "submit" button in the form is clicked
 $('#input-submit').on('click', function() {
-         //store the form submissions entry in a variable. Each of these variables are being pulled from the ID we assigned the input fields in the form, via HTML.
+         //Store the form submissions entry in variables 
          var $season = capitolFirstLetter( $('#input-season').val() );
          var $person = capitolFirstLetter( $('#input-person').val() );
          var $organization = capitolFirstLetter( $('#input-organization').val() );
@@ -15,27 +19,27 @@ $('#input-submit').on('click', function() {
          var $age = capitolFirstLetter( $('#input-age').val() );
          //set up our 'correct' variable:
          var correct = 0;
+         //set variable that will output the result
+		 var result = "";
+		 //Holds score of the quiz
+		 var quizscore = '';
+
          //uncomment this alert to test that the variables are being pulled correctly. When we click submit it should output the variables to the alert box. The  + ' ' +  is just so there's a space between the words.
          //console.log($season + $person + $organization  +  $doublecross  + $age);
       
         // start comparing these variables with the correct answers so we can prepare the total score
-        //this is the same function from the original code, just replaced with the variables from above.
         if ( $season === 'Autumn' || $season === 'Fall' ) {
 			correct = correct + 1 ;
 		}
-
 		if ( $person === 'Kat' || $person === 'Kathrine' || $person === 'Katie' || $person === 'Kt' ) {
 			correct = correct + 1 ;
 		}
-
         if ( $organization === 'Bromandos' || $organization === 'The Bromandos' || $organization === 'Bromando' ) {
 			correct = correct + 1 ;
 		}
-
         if ( $doublecross === 'No' ) {
 			correct = correct + 1 ;
 		}
-
         if ( $age === '7' || $age === 'Seven' ) {
 			correct = correct + 1 ;
 		}
@@ -44,17 +48,12 @@ $('#input-submit').on('click', function() {
 		//console.log(correct);
 
 		// formats results alert to have proper grammar depending on user's number of correct answers
-		
-		var quizscore = '';
-
 		if (correct === 1) {
 			quizscore = 'You correctly answered 1 question.';
 		} else {
 			quizscore = 'You correctly answered ' + correct +' questions.';
 		}
 
-		//set variable that will output the result
-		var result = "";
 		// change the 'result' variable so that it ranks user depending on number of correct answers
 		if (correct === 5) {
 			result = "You must know veryspry. You have been awarded a golden crown.";
@@ -70,19 +69,20 @@ $('#input-submit').on('click', function() {
 		//console.log(quizscore);
 		//.log(result);
 
-		//this function hides the html form that holds all of the questions
+		//Hides the HTML form that holds all of the questions
         $( "#questions-form" ).hide();
 		// this function shows the two variables 'quizscore' and 'result' that was set above
 		$("#quiz-score").append(
-			'<div id="quizresults"><h2>' + quizscore + 
+			'<h2>' + quizscore + 
 		    '</h2> <br> <p> ' + result + 
-		    '<br> <button id="results" name="button">Show Correct Answers</button> </div>');
+		    '<br> <button id="results" name="button">Show Correct Answers</button>'
+		 );
 		
 		// Used for more troubleshooting
-		// alert (quizscore + correct);
+		//alert (quizscore + correct);
 });   
 
-//This shows the correct answers. This function fires when the button with ID 'results' is clicked. This button was appended at the end of the last function.
+//This shows the correct answers.
 $(document).on('click', '#results', function(){
 
 	//Redefines user input variables locally so that they can be used to show final results
@@ -91,16 +91,33 @@ $(document).on('click', '#results', function(){
          var $organization = capitolFirstLetter( $('#input-organization').val() );
          var $doublecross = capitolFirstLetter( $('#input-doublecross').val() );
          var $age = capitolFirstLetter( $('#input-age').val() );
-	//Hide our quiz results div again
-	$( "#quizresults" ).hide();
+	//Hide quiz-score div
+	$( "#quiz-score" ).empty();
 	//Append user the users answers compared to correct answers onto the page
-	$(".container").append(
+	$("#quiz-results").append(
 		'<div id="correctanswers"> <h2>Your Answers --  Correct Answers</h2> <br> <p> Question one - your answer = ' + 
 		$season + ' - correct answer = Autumn <br> Question two - your answer = ' + 
 		$person + ' - correct answer = Kat <br> Question three - your answer = ' + 
 		$organization + ' - correct answer = The Bromandos <br> Question four - your answer = ' + 
 		$doublecross + ' - correct answer = No <br> Question five - your answer = ' + 
 		$age + ' - correct answer = Seven <br> </p> </div>');
+	$('#reset').show();
+});
+
+	//Reset the form when the Reset button is clicked
+	$(document).on( 'click', '#reset', function() {
+		//Redefine variables as empty strings and clear HTML form of previous user answers
+         var $season = $('#input-season').val('');
+         var $person = $('#input-person').val('');
+         var $organization = $('#input-organization').val('');
+         var $doublecross = $('#input-doublecross').val('');
+         var $age = $('#input-age').val('');
+	//Clears score and result divs
+	$('#quiz-results').empty();
+	//Hides reset button again so that it doesn't show while the HTML form is showing
+	$('#reset').hide();
+	//Unhides the user input div and accompanying HTML form
+	$('#questions-form').show();
 });
 		
 });
