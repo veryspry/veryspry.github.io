6a74e1cf47ef3bc0d4c52bebcf3c775e4a745a79
veryspry <ehlinger.matt@gmail.com>
2017-11-22 12:01:43 -0600

    remove extraneous folder

--- veryspry.github.io-master2/assets/js/relationship.js
+++
@@ -1,5 +0,0 @@
-var status = prompt("What is the status of your relationship?");
-var message = "Your relationship is " +status+ " at this current moment. ";
-message += "Are you satisfied with this result? ";
-message += "Please refresh the page and type in a new answer if you are not."
-document.write(message);
